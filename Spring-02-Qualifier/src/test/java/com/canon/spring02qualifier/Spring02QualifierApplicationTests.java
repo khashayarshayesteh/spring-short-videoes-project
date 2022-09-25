bc35d2dd81194b78990a8460f71961fde14df777
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-24 18:43:25 -0700

    second commit Qualifier

---
+++ Spring-02-Qualifier/src/test/java/com/canon/spring02qualifier/Spring02QualifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canon.spring02qualifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring02QualifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
